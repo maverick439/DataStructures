1ec9ebf7d8dea3dbae0fa998b5bb1db8e4dd3879
maverick439 <srijan439@yahoo.com>
2018-08-07 17:23:04 +0530

    Update SetBits.cpp

--- Bitmasking/SetBits.cpp
+++ Bitmasking/SetBits.cpp
@@ -1,5 +1,5 @@
 //To find the no of 1s in the binary expression
-
+//To find the set bits 
 #include <iostream>
 using namespace std;
 
@@ -12,3 +12,17 @@ int main() {
     cout<<ans;
 
 }
+
+//Alternate method //Better solution
+/*
+int main() {
+    int n;
+    cin>>n;
+    int count = 0;
+    while(n>0){
+        n = n & (n-1);
+        count++;
+    }
+    cout<<count;
+    return 0;
+}*/
